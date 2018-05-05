6abb976ff79f6aa81346bff76fd38918cd757651
Brad Marshall <bmarshall@ollon.us>
2018-05-05 04:11:20 -0500

    updates

---
+++ src/Setup/PSInstaller/Script.ps1
@@ -0,0 +1,12 @@
+﻿$sb = [System.Text.StringBuilder]::new()
+$sw = [System.IO.StringWriter]::new($sb)
+$w  = [System.CodeDom.Compiler.IndentedTextWriter]::new($sw)
+$w.Indent = 1
+Get-ChildItem -File -Path 'C:\Stage\git\PowerShellModules\bin\Debug\Infrastructure\PowerShell.Infrastructure' | ForEach-Object -Process {
+    $File = $_
+    $FileName = "FILE_$($File.Name.Replace('.',''))"
+    $w.WriteLine("        <File Id=`"$FileName`"")
+    $w.WriteLine("              Source=`"`$(var.Infrastructure.TargetDir)\PowerShell.Infrastructure\$($File.Name)`" />")
+}
+$sb.ToString() | Set-Clipboard
+$sb.ToString()
